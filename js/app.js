069cf939c8e3acf73cdd4e9784cc15a2e8619a87
Ivann <ivann.galic@edu.itescia.fr>
2020-01-17 09:57:37 +0100

    push contact modif

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
-/* Displays the window menu when the button menu is clicked*/
 $(document).ready(function () {
     $('.boutonNavigation').click(function () {
-        $(this).find('.barre').toggleClass('black'); /* Change the color of the button menu to black when the menu is open*/
-        $('.navigation').toggleClass('isOpen'); /* Class the open state*/
+        $(this).find('.barre').toggleClass('black');
+        $('.navigation').toggleClass('isOpen');
     });
 });
